a45c80adf0e35cef23ef482f4d48b3be10b427d7
HiterretiH <47186981+HiterretiH@users.noreply.github.com>
2025-02-25 01:16:30 +0300

    Implement Create Location component

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -2,27 +2,39 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-
-export interface LocationJson {
-    id: number;
-    x: number;
-    y: number;
-    z?: number;
-}
+import { AuthService } from './auth.service';
+import { LocationJson } from '../json';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class LocationService {
-    private apiUrl = `${environment.apiUrl}/locations`;
+  private apiUrl = `${environment.apiUrl}/locations`;
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
-    getLocations(): Observable<LocationJson[]> {
-        return this.http.get<LocationJson[]>(this.apiUrl);
-    }
+  getLocations(): Observable<LocationJson[]> {
+    this.authService.updateToken();
+    return this.http.get<LocationJson[]>(`${this.apiUrl}/list`, { headers: this.authService.headers });
+  }
 
-    createLocation(location: LocationJson): Observable<LocationJson> {
-        return this.http.post<LocationJson>(this.apiUrl, location);
-    }
+  getById(id: number): Observable<LocationJson> {
+    this.authService.updateToken();
+    return this.http.get<LocationJson>(`${this.apiUrl}/get/${id}`, { headers: this.authService.headers });
+  }
+
+  createLocation(location: LocationJson): Observable<LocationJson> {
+    this.authService.updateToken();
+    return this.http.post<LocationJson>(`${this.apiUrl}`, location, { headers: this.authService.headers });
+  }
+
+  updateLocation(id: number, location: LocationJson): Observable<LocationJson> {
+    this.authService.updateToken();
+    return this.http.put<LocationJson>(`${this.apiUrl}/update/${id}`, location, { headers: this.authService.headers });
+  }
+
+  deleteLocation(id: number): Observable<void> {
+    this.authService.updateToken();
+    return this.http.delete<void>(`${this.apiUrl}/delete/${id}`, { headers: this.authService.headers });
+  }
 }
